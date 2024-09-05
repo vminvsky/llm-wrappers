9cc20712fca93590ebf7868c78b97edbb75fd538
Veniamin Veselovsky <venia@veselovsky.org>
2024-09-05 19:54:57 -0400

    fixing to make it pip installable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='llm_wrappers',
     version='0.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=[],
+    packages=find_packages('src'),  # Finds all packages inside 'src'
+    package_dir={'': 'src'},        # Tells setuptools that packages are under 'src'
+    install_requires=[],             # Add any dependencies if necessary
 )
---
+++ src/llm_wrappers/models/__init__.py
@@ -0,0 +1,6 @@
+from .anthropic_model import AnthropicModel
+from .openai_model import OpenAIModel
+from .google_model import GoogleModel
+from .cohere_model import CohereModel
+from .togetherai_model import TogetherAIModel
+from .model_utils import HumanMessage, SystemMessage, AIMessage
